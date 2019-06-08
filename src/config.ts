d1b05d276a688cf7613951f98db381c1d5684f0f
ES <es019@protonmail.com>
2019-06-08 17:15:20 +0700

    Update tests.

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,6 @@
 import puppeteer from 'puppeteer';
 
 export const PORT = 8080;
-export const TIMEOUT = 1.5 * 60 * 1000;
 export const TESTURL = 'https://steamcommunity.com/app/323190/discussions/1/';
 export const PERPAGE = [15, 30, 50];
 
--- src/tests/topic.test.ts
+++ src/tests/topic.test.ts
@@ -3,57 +3,58 @@ import * as scraper from '../scraper';
 import * as config from '../config';
 import * as parser from '../parser';
 
-let topicList: Element[];
-let topicElement: Element;
+let topics: Element[];
+let topic: Element;
 
 beforeAll(async () => {
-  topicList = await scraper.scrapeTopics({
+  topics = await scraper.scrapeTopics({
     testing: true,
     url: config.TESTURL,
   });
-  topicElement = topicList[0];
-}, config.TIMEOUT);
+  topic = topics[0];
+}, config.navigationTimeout);
 
-test(`Topic list of ${config.TESTURL} has been retrieved`, async () => {
-  const expected = topicList.length > 0;
-  expect(expected).toBeTruthy();
-}, config.TIMEOUT);
+describe('Scrape, parse and compile topic element', () => {
+  test(`Topic list of ${config.TESTURL} has been retrieved`, () => {
+    const expected = topics.length > 0;
+    expect(expected).toBeTruthy();
+  });
 
-describe('Parse data from scraped topic element', () => {
   test(`Title has been parsed`, () => {
-    const content = parser.getTopicTitle(topicElement);
+    const content = parser.getTopicTitle(topic);
     expect(typeof content).toBe('string');
   });
 
   test(`Author has been parsed`, () => {
-    const content = parser.getTopicAuthor(topicElement);
+    const content = parser.getTopicAuthor(topic);
     expect(typeof content).toBe('string');
   });
 
   test(`Timestamp has been parsed`, () => {
-    const content = parser.getTopicTimestamp(topicElement);
+    const content = parser.getTopicTimestamp(topic);
     expect(typeof content).toBe('number');
   });
 
   test(`Reply count has been parsed`, () => {
-    const content = parser.getTopicReplyCount(topicElement);
+    const content = parser.getTopicReplyCount(topic);
     expect(typeof content).toBe('number');
   });
 
   test(`Tooltip has been parsed`, () => {
-    const content = parser.getTopicTooltip(topicElement);
+    const content = parser.getTopicTooltip(topic);
     expect(typeof content).toBe('string');
   });
-});
 
-test(`'Topic' object has been compiled`, () => {
-  const topic = compileTopic(topicElement);
-  expect(typeof topic.pinned).toBe('boolean');
-  expect(typeof topic.locked).toBe('boolean');
-  expect(typeof topic.answered).toBe('boolean');
-  expect(typeof topic.title).toBe('string');
-  expect(typeof topic.author).toBe('string');
-  expect(typeof topic.timestamp).toBe('number');
-  expect(typeof topic.replyCount).toBe('number');
-  expect(typeof topic.tooltip).toBe('string');
+  test(`'Topic' object has been compiled`, () => {
+    const compiledTopic = compileTopic(topic);
+
+    expect(typeof compiledTopic.pinned).toBe('boolean');
+    expect(typeof compiledTopic.locked).toBe('boolean');
+    expect(typeof compiledTopic.answered).toBe('boolean');
+    expect(typeof compiledTopic.title).toBe('string');
+    expect(typeof compiledTopic.author).toBe('string');
+    expect(typeof compiledTopic.timestamp).toBe('number');
+    expect(typeof compiledTopic.replyCount).toBe('number');
+    expect(typeof compiledTopic.tooltip).toBe('string');
+  });
 });
